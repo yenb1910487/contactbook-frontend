57ffecd809060f0ce9bab8e6a10be9174ea7e674
yenb1910487 <yenb1910487@student.ctu.edu.vn>
2022-10-29 15:25:06 +0700

    Hien thi danh sach cac contact lay ve tu server

---
+++ src/router/index.js
@@ -0,0 +1,30 @@
+import { createWebHistory, createRouter } from "vue-router";
+import ContactBook from "@/views/ContactBook.vue";
+
+const routes = [
+    {
+        path: "/",
+        name: "contactbook",
+        component: ContactBook,
+    },
+    
+        {
+            path: "/contacts/:id",
+            name: "contact.edit",
+            component: () => import("@/views/ContactEdit.vue"),
+            props: true // Truyền các biến trong $route.params vào làm props
+            },
+            {
+                path: "/add",
+                name: "contact.add",
+                component: () => import('@/views/ContactAdd.vue'),
+                props: true
+            }
+];
+
+const router = createRouter({
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes,
+});
+
+export default router;
---
+++ src/views/ContactAdd.vue
@@ -0,0 +1,41 @@
+<script>
+import ContactForm from "@/components/ContactForm.vue";
+import ContactService from "@/services/contact.service";
+export default {
+  components: { ContactForm },
+ 
+  data() {
+    return {
+      contact: {
+        _id: "",
+        address: "",
+        email: "",
+        favorite: null,
+        name: "",
+        phone: "",
+      },
+      message: "",
+    };
+  },
+  methods: {
+    async addContact(data) {
+      try {
+        await ContactService.create(data);
+        this.message = "Thêm liên hệ thành công!!!";
+      } catch (error) {
+        console.log(error);
+      }
+    },
+  },
+};
+</script>
+
+<template>
+  <div class="page">
+    <h4>Thêm  liên hệ</h4>
+    <ContactForm :contact="contact" @submit:contact="addContact" />
+    <p>{{ message }}</p>
+  </div>
+</template>
+
+<style></style>
